1193ffee091cf406e707167e58d5bec0d72b4ba1
Greedysky <greedysky@163.com>
2021-06-26 20:17:41 +0800

    Bump version 2.2.0.0 Final

--- TTKCommon/ttkversion.h
+++ TTKCommon/ttkversion.h
@@ -19,17 +19,17 @@
  * with this program; If not, see <http://www.gnu.org/licenses/>.
  ================================================= */
 
-//update time 2020.07.05
-#define TTKTOOLS_VERSION_STR    "2.1.0.0"
-#define TTKTOOLS_VERSION_WSTR   L"2.1.0.0"
-#define TTKTOOLS_VER_TIME_STR   "(2020/07/05)"
+//update time 2021.06.26
+#define TTKTOOLS_VERSION_STR    "2.2.0.0"
+#define TTKTOOLS_VERSION_WSTR   L"2.2.0.0"
+#define TTKTOOLS_VER_TIME_STR   "(2021/06/26)"
 
 #define TTKTOOLS_MAJOR_VERSION 2
-#define TTKTOOLS_MIDLE_VERSION 1
+#define TTKTOOLS_MIDLE_VERSION 2
 #define TTKTOOLS_MINOR_VERSION 0
 #define TTKTOOLS_PATCH_VERSION 0
 
-#define TTKTOOLS_VERSION 0x02100
+#define TTKTOOLS_VERSION 0x02200
 
 #define TTK_VERSION_CHECK(major, middle, minor, patch) ((major<<12)|(middle<<8)|(minor<<4)|(patch))
 
