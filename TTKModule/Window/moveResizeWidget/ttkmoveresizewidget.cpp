979a524e3362403cc94efdb5dcaba0fba9806665
Greedysky <greedysky@163.com>
2022-09-21 14:03:48 +0800

    fix: Remove block window resize option on linux[665404]

--- TTKModule/Window/moveResizeWidget/ttkmoveresizewidget.cpp
+++ TTKModule/Window/moveResizeWidget/ttkmoveresizewidget.cpp
@@ -71,9 +71,7 @@ void TTKMoveResizeWidget::mousePressEvent(QMouseEvent *event)
 void TTKMoveResizeWidget::mouseMoveEvent(QMouseEvent *event)
 {
     QWidget::mouseMoveEvent(event);
-#ifndef Q_OS_UNIX
     !m_struct.m_isPressBorder ? sizeDirection() : moveDirection();
-#endif
 
     if(m_struct.m_mouseLeftPress)
     {
