1935fe668704f826e4042450ddc44faadb93b2ce
Greedysky <greedysky@163.com>
2022-11-23 14:08:22 +0800

    feat: Add ttkcommandline common library module[048221]

--- TTKExample/main.cpp
+++ TTKExample/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
     QTextCodec::setCodecForLocale(codec);
 #if !TTK_QT_VERSION_CHECK(5,0,0)
     QTextCodec::setCodecForCStrings(codec);
