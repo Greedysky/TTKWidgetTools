c45dbec590908c20fcfa61a57ca01a785ed470e6
greedysky <greedysky@163.com>
2023-10-14 14:26:06 +0800

    refactor: Optimize global project module[952014]

--- TTKCommon/ttkversion.h
+++ TTKCommon/ttkversion.h
@@ -20,20 +20,22 @@
  ***************************************************************************/
 
 //update time 2023.04.15
-#define TTK_VERSION             0x02700
-#define TTK_VERSION_STR         "2.7.0.0"
-#define TTK_VERSION_TIME_STR    "(2023/04/15)"
-
 #define TTK_MAJOR_VERSION 2
 #define TTK_MIDLE_VERSION 7
 #define TTK_MINOR_VERSION 0
 #define TTK_PATCH_VERSION 0
 
-#define TTK_RC_FILEVERSION      2,7,0,0
+#define TTK_VERSION             TTK_VERSION_CHECK(TTK_MAJOR_VERSION, TTK_MIDLE_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION)
+#define TTK_VERSION_STR         TTK_VERSION_CHECK_STR(TTK_MAJOR_VERSION, TTK_MIDLE_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION)
+#define TTK_VERSION_TIME_STR    "(2023/04/15)"
+
+#define TTK_RC_FILEVERSION      TTK_MAJOR_VERSION, TTK_MIDLE_VERSION, TTK_MINOR_VERSION, TTK_PATCH_VERSION
 #define TTK_RC_PRODUCTVERSION   TTK_VERSION_STR
 #define TTK_RC_COMPANY          "Greedysky Studio, Ltd."
 #define TTK_RC_COPYRIGHT        "Copyright (C) 2023 Greedysky Studio"
 
-#define TTK_VERSION_CHECK(major, middle, minor, patch) ((major<<12)|(middle<<8)|(minor<<4)|(patch))
+#define TTK_VERSION_CHECK(major, middle, minor, patch)        ((major<<12)|(middle<<8)|(minor<<4)|(patch))
+#define TTK_VERSION_CHECK_STR(major, middle, minor, patch)    TTK_VERSION_CHECK_STR_I(major, middle, minor, patch)
+#define TTK_VERSION_CHECK_STR_I(major, middle, minor, patch)  #major "." #middle "." #minor "." #patch
 
 #endif // TTKVERSION_H
