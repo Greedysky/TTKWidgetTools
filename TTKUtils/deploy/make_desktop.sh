33721a604b29e6495c85b3616a850d8a6f2ca831
Greedysky <greedysky@163.com>
2024-11-29 16:19:30 +0800

    feat: Add new cmake module[067313]

---
+++ TTKUtils/deploy/make_desktop.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+path=share/applications
+if [ ! -d $path ]; then
+  mkdir $path
+fi
+
+datapath=$path/TTKWidgetTools.desktop
+packpath="$PWD/$datapath"
+userpath=~/.local/$datapath
+iconpath="$PWD/share/pixmaps/ttkwidgettools.png"
+execpath="$PWD/../TTKWidgetTools"
+
+echo -n "[Desktop Entry]
+Type=Application
+Version=2.8.0.0
+Name=TTKWidgetTools
+Name[zh_CN]=QWidget自定义控件集合
+Name[zh_TW]=QWidget自定義控件集合
+GenericName=TTKWidgetTools
+GenericName[zh_CN]=QWidget自定义控件集合
+GenericName[zh_TW]=QWidget自定義控件集合
+Comment=TTKWidgetTools
+Comment[zh_CN]=QWidget自定义控件集合
+Comment[zh_TW]=QWidget自定義控件集合
+Icon=$iconpath
+Exec=$execpath
+Terminal=false
+Keywords=widget;
+Categories=widget;Qt;
+MimeType=
+X-KDE-StartupNotify=false\n" > $packpath
+
+cp -rv $packpath $userpath
+chmod +x $userpath
---
+++ TTKUtils/resource/README.txt
@@ -0,0 +1,11 @@
+QWidget自定义控件集合
+TTKWidgetTools
+
+Offical web page: https://github.com/Greedysky/TTKWidgetTools
+Bug tracker: https://github.com/Greedysky/TTKWidgetTools/issues
+Maintainer: Greedysky <greedysky@163.com>
+License: GNU Lesser General Public License Version 3 for TTKCommon modules
+         GNU Lesser General Public License Version 3 for other modules
+
+本软件仅供学习交流，不可用于商业用途，否则后果自负
+Copyright(C) 2015 - 2024 Greedysky All Rights Reserved
