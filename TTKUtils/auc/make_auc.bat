7a282f3f92641f3406df6df233ec9a09d636b02f
Greedysky <greedysky@163.com>
2024-12-05 12:19:06 +0800

    Bump version 3.0.0.0 Final

--- TTKUtils/auc/make_auc.bat
+++ TTKUtils/auc/make_auc.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\mt.exe" -manifest "manifest" -outputresource:"4.1.1.0/TTKWidgetTools.exe"
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\mt.exe" -manifest "manifest" -outputresource:"3.0.0.0/TTKWidgetTools.exe"
--- TTKUtils/linux_time.sh
+++ TTKUtils/linux_time.sh
@@ -1,2 +1,2 @@
 #!/bin/sh  
-find . -exec touch -c -m -d "2024-03-28 12:00" {} \;
+find . -exec touch -c -m -d "2024-12-05 12:00" {} \;
