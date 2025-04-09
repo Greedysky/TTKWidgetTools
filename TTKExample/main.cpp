5ead64687cd0698e803cf219dc4a1f4fa734d746
Greedysky <greedysky@163.com>
2025-04-09 17:47:20 +0800

    refactor: Optimize ttk library module[053778]

--- TTKExample/main.cpp
+++ TTKExample/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     if(app.isRunning())
     {
         TTK_INFO_STREAM("One app has already run");
-        return -1;
+        return EXIT_FAILURE;
     }
 
     QTextCodec *codec = QTextCodec::codecForName("UTF-8");
