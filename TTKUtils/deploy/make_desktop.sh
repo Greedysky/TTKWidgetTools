5c0180804e463f455d28e1593b17547a293ada0d
Greedysky <greedysky@163.com>
2025-06-15 16:20:25 +0800

    refactor: Clean code[867331]

--- TTKUtils/deploy/make_desktop.sh
+++ TTKUtils/deploy/make_desktop.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-path=share/applications
-if [ ! -d $path ]; then
-  mkdir $path
-fi
-
 curpath=$PWD/deploy
 if [ $# -eq 0 ]; then
   curpath=$PWD
 fi
 
-datapath=$path/TTKWidgetTools.desktop
-packpath="$curpath/$datapath"
-userpath=~/.local/$datapath
+path=$curpath/share/applications
+if [ ! -d $path ]; then
+  mkdir $path
+fi
+
+packpath=$path/TTKWidgetTools.desktop
+userpath=~/.local/share/applications
 iconpath="$curpath/share/pixmaps/ttkwidgettools.png"
 execpath="$curpath/../TTKWidgetTools"
 
