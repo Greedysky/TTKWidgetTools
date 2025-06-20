df0a397704d0fb6e9a015e16da4dd0aae4e348bd
Greedysky <greedysky@163.com>
2025-06-20 17:03:29 +0800

    docs: Update resource document[864014]

--- TTKUtils/deploy/make_desktop.sh
+++ TTKUtils/deploy/make_desktop.sh
@@ -29,11 +29,11 @@ echo -n "[Desktop Entry]
 Type=Application
 Version=${packvern}
 Name=TTKWidgetTools
-Name[zh_CN]=QWidget自定义控件集合
-Name[zh_TW]=QWidget自定義控件集合
+Name[zh_CN]=TTK控件集合
+Name[zh_TW]=TTK控件集合
 GenericName=TTKWidgetTools
-GenericName[zh_CN]=QWidget自定义控件集合
-GenericName[zh_TW]=QWidget自定義控件集合
+GenericName[zh_CN]=TTK控件集合
+GenericName[zh_TW]=TTK控件集合
 Comment=TTKWidgetTools
 Comment[zh_CN]=QWidget自定义控件集合
 Comment[zh_TW]=QWidget自定義控件集合
